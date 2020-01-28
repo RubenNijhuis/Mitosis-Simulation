c73df9a0b8cc35a2e793852c8da6e53c4616d7aa
rubennijhuis <contact@rubenswebsite.com>
2020-01-28 15:07:48 +0100

    update

---
+++ cell.js
@@ -0,0 +1,48 @@
+class Cell {
+    constructor(x, y, d, color, stroke) {
+        this.x = x;
+        this.y = y;
+
+        this.velocity = {
+            x: Math.random() - 0.5,
+            y: Math.random() - 0.5
+        };
+        this.diameter = d;
+        this.color = color;
+        this.stroke = stroke;
+        this.lineWidth = 5 * (this.diameter / 40);
+        this.maxD = 60;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.diameter, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+        // c.lineWidth = this.lineWidth;
+        // c.strokeStyle = this.stroke;
+        // c.stroke();
+        c.closePath();
+    }
+
+    update() {
+        if (this.x - this.diameter <= 0 || this.x + this.diameter >= innerWidth) {
+            this.velocity.x = -this.velocity.x;
+        } else if (this.y + this.diameter > innerHeight || this.y - this.diameter < 0) {
+            this.velocity.y = -this.velocity.y;
+        } else {
+            this.x += this.velocity.x;
+            this.y += this.velocity.y;
+        }
+
+        this.draw();
+    }
+
+    mitosis() {
+        this.x += randomIntFromRange(-this.diameter / 3, this.diameter / 3);
+        this.y += randomIntFromRange(-this.diameter / 3, this.diameter / 3);
+        let r2 = ((Math.PI * (this.diameter / 2) ** 2) / 2 / Math.PI) ** (1 / 2) * 2;
+
+        return new Cell(this.x, this.y, r2, this.color);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,35 +1,55 @@
-const startCells = 100;
+const button = document.querySelector("button");
+const overlay = document.querySelector("#overlay");
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+const startCells = 10;
 let totalCells = [];
 
-// Draws the canvas ands pushes cells to the array as many times as 'startCells' defines
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    for (let i = 0; i < startCells; i++) {
-        totalCells.push(new Cell());
-    }
-}
+const distance = (x1, y1, x2, y2) => Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+const randomIntFromRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-// For each cell, run the class methods
-function draw() {
-    background(30);
-    noStroke();
-    totalCells.forEach(cell => {
-        cell.move();
-        cell.show();
-    });
-}
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-/*
-  It is a backwards loop because,
-  every new cell would be added to the end,
-  which would mess up the iteration
-*/
-function mousePressed() {
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    init();
+});
+
+button.addEventListener("click", () => init());
+
+overlay.addEventListener("click", (e) => {
     for (let i = totalCells.length - 1; i >= 0; i--) {
-        if (totalCells[i].clicked(mouseX, mouseY)) {
+        if (distance(totalCells[i].x, totalCells[i].y, e.x, e.y) < totalCells[i].diameter) {
             totalCells.push(totalCells[i].mitosis());
             totalCells.push(totalCells[i].mitosis());
             totalCells.splice(i, 1);
+            break;
         }
     }
-}
+});
+
+const init = () => {
+    totalCells = [];
+    for (let i = 0; i < startCells; i++) {
+        const radius = randomIntFromRange(80, 100);
+        let x = randomIntFromRange(radius, canvas.width - radius);
+        let y = randomIntFromRange(radius, canvas.height - radius);
+        totalCells.push(new Cell(x, y, radius, `rgba(230, 199, 232, 0.75)`, `rgba(215, 119, 221)`));
+    }
+};
+
+const animate = () => {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    c.fillStyle = "rgb(244, 237, 237)";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    for (let cell of totalCells) {
+        cell.update(totalCells);
+    }
+};
+
+init();
+animate();
